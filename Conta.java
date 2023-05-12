239dcc4cd29709d56a7c48560f0831279939f7cd
Lohan Silva <133441351+Lohan1@users.noreply.github.com>
2023-05-12 20:39:24 -0300

    Codigo de Conta de Banco

---
+++ Conta.java
@@ -0,0 +1,28 @@
+public class Conta {
+
+    public int numero;
+    public String NomeAgencia;
+    public int NumeroAgencia;
+    private String NomeBanco;
+    private int NumeroBanco;
+    private String TipoConta;
+    private float Saldo;
+
+    public void abrir() {
+    }
+
+    public void encerrar() {
+    }
+
+    public void consultar() {
+    }
+
+    public void saldo() {
+    }
+
+    public void creditar() {
+    }
+
+    public void debitar() {
+    }
+}
